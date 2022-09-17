90f82a143ce2606db96faee4f65aaf125cc68261
Sean <craig.sean92@gmail.com>
2022-09-16 22:59:02 -0600

    test push

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,13 +2,11 @@ class CommentsController < ApplicationController
 
     def create
         comment = @current_user.comments.create!(post_id: params[:post_id], body: params[:body])
-        render json: comment, status: :created
     end
 
     def destroy
         @comment = current_user.comments.find_by!(id: params[:id])
         @comment.delete
 
-        render json: @comment, status: :ok
     end
 end
